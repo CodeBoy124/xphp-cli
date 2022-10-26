84f1f63da1845401180f0818492b5b91a1433346
CodeBoy124 <koen7maart@gmail.com>
2022-10-26 15:00:00 +0200

    Added comment

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@ const search = require('./files/search');
 const processXphpFile = require('./processFile/process');
 const writePhpFile = require('./files/write');
 
+// detect all files with .xphp extension and translate them to php
 let xphpFiles = search(process.cwd());
 for (xphpFile of xphpFiles) {
     let translatedToRegularPhp = processXphpFile(xphpFile);
