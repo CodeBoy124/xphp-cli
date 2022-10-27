73a796ea18542baebb82af411884bd6e35ee960f
CodeBoy124 <koen7maart@gmail.com>
2022-10-27 21:07:31 +0200

    Added support for xphp tags which output some inline php

--- bin/processFile/translator.js
+++ bin/processFile/translator.js
@@ -1,14 +1,19 @@
 class Translation {
-    constructor({ from, to = from, hasArguments = true, addOpenBracketAtEnd = true, addSemicolonAtEnd = false, useParenthesesForArguments = true }) {
+    constructor({ from, to = from, hasArguments = true, addOpenBracketAtEnd = true, addSemicolonAtEnd = false, useParenthesesForArguments = true, isInlinePhpTag = false }) {
         this.from = "@" + from;
         this.to = to;
         this.hasArguments = hasArguments;
         this.addOpenBracketAtEnd = addOpenBracketAtEnd;
         this.useParenthesesForArguments = useParenthesesForArguments;
         this.addSemicolonAtEnd = addSemicolonAtEnd;
+        this.isInlinePhpTag = isInlinePhpTag;
     }
     run(args = "") {
-        return `<?php ${this.to}${this.hasArguments ? (this.useParenthesesForArguments ? `(${args})` : ` ${args}`) : ''}${this.addOpenBracketAtEnd ? '{' : ''}${this.addSemicolonAtEnd ? ';' : ''} ?>`;
+        if (this.isInlinePhpTag) {
+            return `<?= ${this.to}${this.hasArguments ? (this.useParenthesesForArguments ? `(${args})` : ` ${args}`) : ''}${this.addOpenBracketAtEnd ? '{' : ''}${this.addSemicolonAtEnd ? ';' : ''} ?>`;
+        } else {
+            return `<?php ${this.to}${this.hasArguments ? (this.useParenthesesForArguments ? `(${args})` : ` ${args}`) : ''}${this.addOpenBracketAtEnd ? '{' : ''}${this.addSemicolonAtEnd ? ';' : ''} ?>`;
+        }
     }
 }
 module.exports = Translation;
