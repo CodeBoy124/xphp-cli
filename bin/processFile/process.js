fcb43afe56ba78a74c09fbd4aea843f1b609c200
CodeBoy124 <koen7maart@gmail.com>
2022-10-28 08:26:47 +0200

    Forgot to remove an import in processFile/process.js

--- bin/processFile/process.js
+++ bin/processFile/process.js
@@ -2,8 +2,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const readJsonFile = require('../files/readJson');
-
 // functions for {{ ... }} syntax
 function processInlinePhp(input) {
     let output = "";
